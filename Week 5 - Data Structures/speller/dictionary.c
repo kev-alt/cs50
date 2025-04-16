bf873c53ae2c286700a1fb23369c66c053d49394
kevser <kevser.altunbey@istanbulticaret.edu.tr>
2025-04-16 22:28:26 +0300

    c

---
+++ Week 5 - Data Structures/speller/dictionary.c
@@ -0,0 +1,94 @@
+#include "dictionary.h"
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+} node;
+
+const unsigned int N = 5000;
+node *table[N];
+unsigned int word_count = 0;
+
+unsigned int hash(const char *word)
+{
+    unsigned int hash = 0;
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        hash = (hash << 2) ^ tolower(word[i]);
+    }
+    return hash % N;
+}
+
+bool check(const char *word)
+{
+    char temp[LENGTH + 1];
+    int len = strlen(word);
+    for (int i = 0; i < len; i++)
+    {
+        temp[i] = tolower(word[i]);
+    }
+    temp[len] = '\0';
+    unsigned int index = hash(temp);
+    node *cursor = table[index];
+    while (cursor != NULL)
+    {
+        if (strcmp(cursor->word, temp) == 0)
+        {
+            return true;
+        }
+        cursor = cursor->next;
+    }
+    return false;
+}
+
+bool load(const char *dictionary)
+{
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        return false;
+    }
+    char word[LENGTH + 1];
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        node *new_node = malloc(sizeof(node));
+        if (new_node == NULL)
+        {
+            fclose(file);
+            return false;
+        }
+        strcpy(new_node->word, word);
+        unsigned int index = hash(word);
+        new_node->next = table[index];
+        table[index] = new_node;
+        word_count++;
+    }
+    fclose(file);
+    return true;
+}
+
+unsigned int size(void)
+{
+    return word_count;
+}
+
+bool unload(void)
+{
+    for (int i = 0; i < N; i++)
+    {
+        node *cursor = table[i];
+        while (cursor != NULL)
+        {
+            node *tmp = cursor;
+            cursor = cursor->next;
+            free(tmp);
+        }
+    }
+    return true;
+}
